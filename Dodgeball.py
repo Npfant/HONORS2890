0d8ecd533f923dbe5162c1b3cf7930c383b88d9e
Npfant <94149801+Npfant@users.noreply.github.com>
2022-04-05 18:11:08 -0500

    Update Dodgeball.py
    Dodgeball now working. Added vibeCheck to cut down on loops taken while navigating to points.

--- Dodgeball.py
+++ Dodgeball.py
@@ -42,20 +42,16 @@ class Dodgeball:
 		rospy.Subscriber('/ball_detector/ball_location', BallLocation, self.measure)
 		rospy.Subscriber('/odom', Odometry, self.handle_pose)
 		
-		self.state = 'search'
+		self.state = 'kick'
 		self.time = rospy.get_time()
 		
-		self.bearing = -1
-		self.distance = -1
-		self.x = -1
-		self.y = -1
-		self.angle = -1
-		self.range = -1
-		self.theta = 0
+		self.bearing, self.distance, self.x, self.y = -1, -1, -1, -1
+		self.angle, self.range, self.theta = -1, -1, 0
+		self.xint, self.yint, self.diff = -1, -1, 1
 		self.bearingpid = PID(320, .01, 0, 0.01, 1)
 		self.distancepid = PID(1.5, -0.35, 0, 0.02, 0.25)
 		self.rangepid = PID(0, -0.35, 0, 0.02, 0.25)
-		self.anglepid = PID(0, 0.01, 0, 0.01, 1)
+		self.anglepid = PID(0, 0.48, 0, 0.01, 1)
 
 	def handle_pose(self, msg):
 		self.x = msg.pose.pose.position.x
@@ -68,6 +64,16 @@ class Dodgeball:
 		bearing = math.atan2(to_y - from_y, to_x - from_x)
 		distance = math.sqrt((from_x - to_x)**2 + (from_y - to_y)**2)
 		return (bearing, distance)
+		
+	def vibeCheck(self, actual, desired):
+		diff = actual - desired
+		if (diff) > math.pi:
+			real_diff = diff % (-2 * math.pi)
+		elif (diff) < -math.pi:
+			real_diff = diff % (2 * math.pi)
+		else:
+			real_diff = diff
+		return (real_diff)
 
 	#def bumped(self, msg):
 		#self.state = 'backward'
@@ -88,63 +94,65 @@ class Dodgeball:
 				twist.angular.z = .5
 				if(self.bearing > 0 and self.distance > 0):
 					self.state = 'approach'
+					
 			if self.state == 'approach':
 				twist.linear.x = 0
 				twist.angular.z = 0
 				if(self.bearing < 0 or self.distance < 0):
 					self.state = 'search' 
 				if(self.bearing > 300 and self.bearing < 340 and self.distance > 1.4 and self.distance < 1.6):
-					self.xint = self.x + 2.12 * math.cos(self.theta)
-					self.yint = self.y + 2.12 * math.sin(self.theta)
-#Need to figure out correct vector calls 
+					self.xint = self.x + 2.12 * math.cos(self.theta - math.pi/4)
+					self.yint = self.y + 2.12 * math.sin(self.theta - math.pi/4)
+					self.xgoal = self.x + 3 * math.cos(self.theta)
+					self.ygoal = self.y + 3 * math.sin(self.theta)
 					self.state = 'navToInt'
 				if(self.bearing > 0 and self.distance > 0):
 					twist.angular.z = self.bearingpid.get_output(self.bearing)
 					twist.linear.x = self.distancepid.get_output(self.distance)
-#Need to verify correct pid calls
-				
-			if self.state == 'kick':
-				twist.linear.x = 1
-				twist.angular.z = 0
-				if(rospy.get_time() - self.time) > 2:
-					self.state = 'search'
 
 			if self.state == 'navToInt':
 				twist.linear.x = 0
 				twist.angular.z = 0
 				self.angle, self.range = self.get_vector(self.x, self.y, self.xint, self.yint)
-				twist.angular.z = self.anglepid.get_output(self.theta - 						self.angle)
+				self.diff = self.vibeCheck(self.theta, self.angle)
+				twist.angular.z = self.anglepid.get_output(self.diff)
 				twist.linear.x = self.rangepid.get_output(self.range)
-				if(self.angle == 0 and self.range == 0):
+				if(abs(self.angle) < 0.75 and abs(self.range) < 0.1):
 					self.state = 'navToKick'
+				print(self.xint,self.yint)
 				
 			if self.state == 'navToKick':
 				twist.linear.x = 0
 				twist.angular.z = 0
-				self.angle, self.range = self.get_vector(self.x, self.y, -1.5 + self.x, 1.5 - self.y)
-				twist.angular.z = self.anglepid.get_output(self.angle)
-				twist.linear.x = self.rangepid.get_output(sef.range)
-				if(self.angle == 0 and self.range == 0):
+				self.angle, self.range = self.get_vector(self.x, self.y, self.xgoal, self.ygoal)
+				self.diff = self.vibeCheck(self.theta, self.angle)
+				twist.angular.z = self.anglepid.get_output(self.diff)
+				twist.linear.x = self.rangepid.get_output(self.range)
+				if(abs(self.angle) < 0.75 and abs(self.range) < 0.1):
 					self.state = 'lineup'
-
+				print(self.xgoal,self.ygoal)
 				
 			if self.state == 'lineup':
 				twist.linear.x = 0
-				twist.angular.z = 0
-				if(self.bearing < 0):
-					self.state = 'search' 
-				if(self.bearing > 300 and self.bearing < 340):
-					self.state = 'kick'
+				twist.angular.z = .5
+				if(self.bearing > 310 and self.bearing < 330):
 					self.time = rospy.get_time()
+					self.state = 'kick'
 				if(self.bearing > 0):
 					twist.angular.z = self.bearingpid.get_output(self.bearing)
+					
+			if self.state == 'kick':
+				twist.angular.z = 0
+				twist.linear.x = 1
+				if(rospy.get_time() - self.time) > 2:
+					self.state = 'search'		
 						
 			print(self.state)
 			#print(self.bearing)
 			#print(self.distance)
-			print(self.x, self.y, self.theta)
-			print(self.angle, self.range)			
-
+			#print(self.x, self.y, self.theta)
+			#print(self.angle, self.range)			
+			
 
 			self.pub.publish(twist)
 			rate.sleep()
